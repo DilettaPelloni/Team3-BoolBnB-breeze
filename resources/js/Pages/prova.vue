9adefb8bd002f7dfb8496353833fa2984ff8ae19
Diletta Pelloni <dilpel@live.it>
2023-04-14 15:06:37 +0200

    breeze per vue

---
+++ resources/js/Pages/prova.vue
@@ -0,0 +1,21 @@
+<script>
+import { usePage } from '@inertiajs/vue3';
+	export default {
+		name:'Prova',
+        data () {
+            return {
+                user: usePage().props.auth.user,
+            };
+        }
+	}
+</script>
+
+<template>
+    Prova
+
+    {{ user }}
+
+</template>
+
+<style scoped>
+</style>
