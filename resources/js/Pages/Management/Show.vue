db3b0ead760b8583e6d464c9f21a5dc1b48f836b
LucaDalRi <euro2xd@gmail.com>
2023-04-20 16:20:00 +0200

    layout base show completato

--- resources/js/Pages/Management/Show.vue
+++ resources/js/Pages/Management/Show.vue
@@ -22,18 +22,20 @@ export default {
 
     <div class="main-container">
       <div class="container-title">
-        <h1>
+        <h1 class="pb-2">
           {{ apartment.title }}
         </h1>
-        <font-awesome-icon :icon="['fas', 'location-dot']"/> {{ apartment.address }}
+        <span>
+          <font-awesome-icon :icon="['fas', 'location-dot']" /> {{ apartment.address }}
+        </span>
       </div>
       <div class="container-img">
         <img :src="apartment.full_cover_img_path" alt="Cover Image Apartment">
       </div>
       <div class="container-info-msg flex justify-between mt-20">
         <div class="container-info flex flex-col justify-between border-b-2">
-          <div class="border-b-2 py-5">
-            <h2>
+          <div class="border-b-2 pb-5">
+            <h2 class="pb-2">
               {{ apartment.title }}
             </h2>
             <span>{{ apartment.rooms }} Stanze &bull; </span>
@@ -41,7 +43,10 @@ export default {
             <span>{{ apartment.bathrooms }} Bagni &bull; </span>
             <span>{{ apartment.size }} M<sup>2</sup></span>
           </div>
-          <div v-for="service in apartment.services">
+          <h2 class="pt-5">
+            Servizi inclusi
+          </h2>
+          <div v-for="service in apartment.services" class="py-3">
             <span>{{ service.icon }}</span>
             <span>{{ service.name }}</span>
           </div>
@@ -50,23 +55,23 @@ export default {
               <h2>
                 Descrizione Appartamento
               </h2>
-              <p>
+              <p class="pt-3 text-gray-600">
                 {{ apartment.description }}
               </p>
             </div>
           </div>
         </div>
         <div class="container-msg">
-          <h2>
+          <h2 class="pb-5">
             Manda un messaggio
           </h2>
           <form action="" method="post">
             <p><label for="name">Nome e Cognome</label></p>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" class="my-4">
             <p><label for="email">La tua E-Mail</label></p>
-            <input type="text" name="email" id="email">
+            <input type="text" name="email" id="email" class="my-4">
             <p><label for="message">Messaggio</label></p>
-            <input type="text" name="message" id="message">
+            <input type="text" name="message" id="message" class="my-4">
             <button type="submit" class="button rounded-full text-white px-4 py-2 mt-8">Invia il messaggio</button>
           </form>
         </div>
@@ -92,9 +97,6 @@ export default {
   .container-title {
     margin-bottom: 50px;
 
-    h1{
-      font-size: 2rem;
-    }
   }
 
   .container-img {
@@ -114,17 +116,26 @@ export default {
   .container-msg {
     width: 30%;
 
-    h2 {
-      font-size: 1.8rem;
-    }
 
     input {
       width: 100%;
     }
 
-    button{
+    button {
       background-color: $main-color;
     }
   }
+}
+
+h1 {
+  font-size: 2rem;
+}
+
+h2 {
+  font-size: 1.8rem;
+}
+
+span{
+  color: rgb(75, 85, 99);
 }
 </style>
