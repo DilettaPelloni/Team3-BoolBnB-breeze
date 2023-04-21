e53e15f2644b9e52df585119bd9bd123de734a15
Diletta Pelloni <dilpel@live.it>
2023-04-21 17:48:37 +0200

    fatti primi filtri

--- resources/js/Pages/Guest/Search.vue
+++ resources/js/Pages/Guest/Search.vue
@@ -8,6 +8,7 @@ export default {
     name: "Index",
     props: {
         apartments: Array,
+        services: Array,
     },
     components: {
         Head,
@@ -18,11 +19,18 @@ export default {
         return {
             rooms: 1,
             beds: 1,
-            bathrooms: 1
+            bathrooms: 1,
+            activeServices: [],
         };
     },
     methods: {
-
+        toggleService(serviceId) {
+            if (this.activeServices.includes(serviceId)) {
+                this.activeServices.splice(this.activeServices.indexOf(serviceId), 1);
+            } else {
+                this.activeServices.push(serviceId);
+            }
+        }, //toggleService
     },
     computed: {
         showApartments() {
@@ -30,6 +38,21 @@ export default {
             apartments = apartments.filter(apartment => apartment.rooms >= this.rooms);
             apartments = apartments.filter(apartment => apartment.beds >= this.beds);
             apartments = apartments.filter(apartment => apartment.bathrooms >= this.bathrooms);
+
+            if (this.activeServices.length > 0) {
+                apartments = apartments.filter((apartment)=> {
+                    let flag = true;
+
+                    let servicesIds = [];
+
+                    apartment.services.forEach((service) => servicesIds.push(service.id));
+
+                    this.activeServices.forEach((service) => {
+                        if(!servicesIds.includes(service)) { flag = false }
+                    });//forEach
+                    return flag;
+                })
+            }
             return apartments;
         },//showApartments
     }
@@ -54,37 +77,64 @@ export default {
 
             <!-- crea una colonna laterale con i filtri all'interno -->
             <div class="flex flex-wrap gap-10">
+                <!-- RAGGIO DI RICERCA -->
                 <div class="flex flex-col w-1/3">
                     <label class="mb-2">Definisci il raggio di ricerca (km)</label>
                     <input type="range" min="1" max="100" value="20" class="slider" id="myRange">
                 </div>
-
+                <!-- ROOMS BATHROOMS BEDS -->
                 <div class="flex justify-center my-10">
+                    <!-- SCATOLA -->
                     <div class="flex flex-wrap gap-10">
+                        <!-- ROOMS -->
                         <div class="flex flex-col w-1/3">
                             <label class="mb-2">Filtro per numero di stanze</label>
                             <select class="rounded-full py-2 px-4 w-full" v-model="rooms">
                                 <option v-for="i in 10" :value="i">{{ i }}</option>
                             </select>
                         </div>
-
+                        <!-- BEDS -->
                         <div class="flex flex-col w-1/3">
                             <label class="mb-2">Filtro per numero di letti</label>
                             <select class="rounded-full py-2 px-4 w-full" v-model="beds">
                                 <option v-for="i in 10" :value="i">{{ i }}</option>
                             </select>
                         </div>
+                        <!-- BATHROOMS -->
                         <div class="flex flex-col w-1/3">
                             <label class="mb-2">Filtro per numero di bagni</label>
                             <select class="rounded-full py-2 px-4 w-full" v-model="bathrooms">
                                 <option v-for="i in 4" :value="i">{{ i }}</option>
                             </select>
                         </div>
-
+                    </div><!-- CHIUSURA SCATOLA -->
+                </div><!-- CHIUSURA ROOMS BATHROOMS BEDS -->
+                <!-- SERVIZI -->
+                <div class="mb-3 pt-[45px]">
+                    <p class="block pb-2 font-medium text-gray-700 text-lg">
+                        Servizi offerti
+                    </p>
+                    {{ activeServices }}
+                    <div class="flex flex-wrap">
+                        <template v-for="service in services">
+                            <label :for="service.id" class="mr-2">
+                                <span class="inline-block font-bold">{{
+                                    service.name
+                                }}</span>
+                                {{ service.icon }}
+                            </label>
+                            <input
+                                type="checkbox"
+                                :id="service.id"
+                                :name="service.id"
+                                :value="service.id"
+                                class="mx-2"
+                                @click="toggleService(service.id)"
+                            />
+                        </template>
                     </div>
-
-                </div>
-            </div>
+                </div> <!-- CHIUSURA SERVIZI -->
+            </div><!-- CHIUSURA CAMPI RICERCA -->
 
 
 
@@ -109,6 +159,11 @@ export default {
                         {{ apartment.rooms }}
                         {{ apartment.beds }}
                         {{ apartment.bathrooms }}
+                        <ul>
+                            <li v-for="service in apartment.services">
+                                {{ service.name }}
+                            </li>
+                        </ul>
                     </div>
                     <!-- CHIUSURA CARD INFO -->
                     </Link>
