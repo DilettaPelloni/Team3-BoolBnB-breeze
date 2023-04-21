0979ed91572ad4d8eb11a494bae3efce416d8c22
MonicaAuditore <mauditore@live.it>
2023-04-21 17:32:51 +0200

    fix_pg messages

--- resources/js/Pages/Management/Messages.vue
+++ resources/js/Pages/Management/Messages.vue
@@ -1,44 +1,71 @@
 <script>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-import { Link } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
 
 export default {
-    name: 'Messages',
+    name: "Messages",
     props: {
         messages: Array,
     },
     components: {
         Head,
         AuthenticatedLayout,
-        Link
-    }
-}
+        Link,
+    },
+};
 </script>
 
 <template>
     <Head :title="Messaggi" />
 
     <AuthenticatedLayout>
-
         <div class="main-container">
-        
+            <h1>I messaggi dell'appartamento:</h1>
             <ul>
                 <li v-for="message in messages">
-                    {{ message.sender_name }}
+                    <div class="utente pb-[20px]">
+                        <div class="nome flex items-center">
+                            <font-awesome-icon
+                                :icon="['fas', 'circle-user']"
+                                style="
+                                    color: rgb(254 91 95 / 78%);
+                                    padding-right: 9px;
+                                "
+                            />
+                            <p><b>Nome: </b>{{ message.sender_name }}</p>
+                        </div>
+                        <div class="mail flex items-center">
+                            <font-awesome-icon
+                                :icon="['fas', 'circle-user']"
+                                style="
+                                    color: rgb(254 91 95 / 78%);
+                                    padding-right: 9px;
+                                "
+                            />
+                            <p><b>Email: </b>{{ message.sender_email }}</p>
+                        </div>
+                    </div>
                 </li>
             </ul>
-
         </div>
-
     </AuthenticatedLayout>
 </template>
 
 <style scoped lang="scss">
 @import "../../../scss/app.scss";
 .main-container {
-  width: 65%;
-  margin: 60px auto;
+    width: 65%;
+    margin: 60px auto;
 }
 
+h1 {
+    font-size: 35px;
+    padding: 50px 0px;
+}
+
+.utente {
+    padding: 50px 0px;
+    border-bottom: 1px solid #fe5b5f;
+}
 </style>
