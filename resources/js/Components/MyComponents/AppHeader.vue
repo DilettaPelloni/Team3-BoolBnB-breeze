761bc9b66814cec4b98c67386d89e14e91e742ad
LucaDalRi <euro2xd@gmail.com>
2023-04-24 12:14:53 +0200

    Creato componente jumbotron

--- resources/js/Components/MyComponents/AppHeader.vue
+++ resources/js/Components/MyComponents/AppHeader.vue
@@ -80,18 +80,7 @@ export default {
             </div>
         </div>
     </div>
-    <div class="container-jumbotron">
-        <!-- video -->
-        <video autoplay muted loop class="video-bg">
-            <source src="/img/sfondo.mp4" type="video/mp4" />
-        </video>
-        <!-- fine video -->
-        <h1 class="text-center">Quale sarà la tua prossima meta?</h1>
-        <h2 class="text-center">Scegli subito la tua destinazione!</h2>
-        <Link :href="route('search')">
-            <button class="button">Cerca la tua meta</button>
-        </Link>
-    </div>
+    
 </template>
 
 <style lang="scss" scoped>
@@ -110,47 +99,4 @@ export default {
     box-shadow: 3px 3px 10px 0px rgba(117, 117, 117, 0.35);
 }
 
-.container-jumbotron {
-    // background-image: url("/img/sfondo-oscurato.jpg");
-    height: 505px;
-    width: 100%;
-    background-position: 36% 67%;
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    gap: 30px;
-    align-items: center;
-
-    position: relative;
-}
-
-.video-bg {
-    position: absolute;
-    top: 0;
-    left: 0;
-    min-width: 100%;
-    min-height: 100%;
-    z-index: -9;
-}
-
-h1 {
-    color: #fff;
-    font-size: 53px;
-}
-
-h2 {
-    color: #fff;
-    font-size: 20px;
-}
-
-.button {
-    background-color: #fe5b5f;
-    padding: 10px 25px;
-    color: #fff;
-    border-radius: 50px;
-    cursor: pointer;
-}
 </style>
