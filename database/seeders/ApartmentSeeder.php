e456ca5ddefc95434a1b312208feedac318e04cc
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-04-25 11:41:05 +0200

    Fatto Seeder Descrizioni Appartamenti

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -69,30 +69,57 @@ class ApartmentSeeder extends Seeder
             //prendo un elemento a caso di ciascun array per formare il titolo
             $title = '';
             do {
-                $adjective = $adjectives[rand(0, (count($adjectives)-1))];
-                $house = $houses[rand(0, (count($houses)-1))];
-                $with = $withs[rand(0, (count($withs)-1))];
-                $title = $adjective.' '.$house.' '.$with;
+                $adjective = $adjectives[rand(0, (count($adjectives) - 1))];
+                $house = $houses[rand(0, (count($houses) - 1))];
+                $with = $withs[rand(0, (count($withs) - 1))];
+                $title = $adjective . ' ' . $house . ' ' . $with;
                 $foundTitle = Apartment::where('title', $title)->first();
             } while ($foundTitle != null);
             $apartment->title = $title;
             $apartment->title_slug = Str::slug($title);
 
-            //descrizione: genera da 2 a 4 paragrafi
-            $apartment->description = $faker->paragraphs(rand(2, 4), true);
+            //DESCRIZIONE
+
+            //prendi il file description dalla cartella public e leggilo
+            $file = fopen(public_path('txt\description.txt'), "r") or die("Unable to open file!");
+            $array = array();
+
+            while (!feof($file)) {
+                $array[] = fgets($file);
+            }
+            fclose($file);
+
+            //componi un nuovo array che ha come oggetti il testo compreso tra una e l'altra virgoletta
+            $array2 = array();
+            foreach ($array as $key => $value) {
+                $array2[] = explode('""', $value);
+            }
+
+            //elimina le virgolette dagli array
+            foreach ($array2 as $key => $value) {
+                foreach ($value as $key2 => $value2) {
+                    $array2[$key][$key2] = str_replace('"', '', $value2);
+                }
+            }
+
+            //descrizione: genera una descrizione prendendo un oggetto dell'array casuale
+            $apartment->description = $array2[rand(0, (count($array2) - 1))][0];
+
+
+            //---FINE DESCRIZIONE----
 
             //numero di stanze
             $apartment->rooms = $faker->numberBetween(1, 10);
             //numero di letti
             do {
                 $apartment->beds = $faker->numberBetween(1, 10);
-            } while($apartment->beds>$apartment->rooms + 1);
+            } while ($apartment->beds > $apartment->rooms + 1);
             //numero di bagni
             do {
                 $apartment->bathrooms = $faker->numberBetween(1, 4);
-            } while($apartment->bathrooms > ($apartment->rooms - $apartment->beds) + 2);
+            } while ($apartment->bathrooms > ($apartment->rooms - $apartment->beds) + 2);
             //m2
-            $apartment->size = ($apartment->rooms * 14) + rand(5,100);
+            $apartment->size = ($apartment->rooms * 14) + rand(5, 100);
 
             //indirizzo
             $address = $faker->address();
@@ -107,7 +134,7 @@ class ApartmentSeeder extends Seeder
             $cover_imgs = scandir(resource_path('seederImg'));
             $cover_imgs = array_diff($cover_imgs, ['.', '..']);
             $cover_img = $cover_imgs[array_rand($cover_imgs)];
-            $apartment->cover_img = 'apartments/'.$cover_img;
+            $apartment->cover_img = 'apartments/' . $cover_img;
 
             //visibile
             $apartment->visible = $faker->numberBetween(0, 1);
