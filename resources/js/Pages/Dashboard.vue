12249bce39eedf352f386f6f47fff115a389fe0d
LucaDalRi <euro2xd@gmail.com>
2023-04-27 10:25:45 +0200

    Eliminato parte sponsor sotto statistiche

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -85,30 +85,6 @@ export default {
                 </div>
             </div>
         </div>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        <button class="button rounded-full text-white px-4 py-2">
-                            <Link :href="route('sponsorship')">Sponsorizza un appartamento</Link>
-                        </button>
-                    </div>
-                    <div class="p-6 text-gray-900 flex justify-between">
-                        <div>
-                            nome appartamento
-                        </div>
-                        <div class="flex">
-                            <div class="pr-3">
-                                sponsor attiva/scaduta
-                            </div>
-                            <div>
-                                data scadenza / v-if rinnova btn
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </AuthenticatedLayout>
 </template>
 
