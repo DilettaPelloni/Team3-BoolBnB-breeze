51fa5a9dbab2f9a2c65fe743211796095903f1b0
LucaDalRi <euro2xd@gmail.com>
2023-04-28 16:35:03 +0200

    aggiustato un po' breakpoints header

--- resources/js/Components/MyComponents/AppHeader.vue
+++ resources/js/Components/MyComponents/AppHeader.vue
@@ -24,59 +24,34 @@ export default {
 </script>
 
 <template>
-    <div
-        style="border-bottom: 1px solid pink"
-        class="container-header flex justify-between items-center py-[10px] px-[185px] 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col"
-    >
-        <div class="container-img h-[50px] w-[250px]">
+    <div style="border-bottom: 1px solid pink"
+        class="container-header flex justify-between items-center py-[10px] md:px-[185px] sm:px-[100px] 2xl:flex-row xl:flex-row lg:flex-row">
+        <div class="container-img h-[50px]">
             <Link :href="route('welcome')">
-                <img
-                    src="/img/LogoBoolBnB.png"
-                    alt="Logo BoolBnB"
-                    class="object-contain h-full"
-                />
+            <img src="/img/LogoBoolBnB.png" alt="Logo BoolBnB" class="object-contain h-full" />
             </Link>
         </div>
         <div class="flex flex-col">
             <div class="container-login ml-5 relative">
-                <font-awesome-icon
-                    :icon="['fas', 'bars']"
-                    @click="toggleVisibilty"
-                />
-                <font-awesome-icon
-                    :icon="['fas', 'circle-user']"
-                    style="color: rgb(254 91 95 / 78%); padding-left: 9px"
-                    size="2xl"
-                    @click="toggleVisibilty"
-                />
-                <!-- <font-awesome-icon :icon="['fas', 'bars']" /> -->
-                <div
-                    class="absolute z-50 rounded-md shadow-lg w-48 origin-top-right right-0 text-right p-3 bg-white mt-3"
-                    v-if="menuVisibility"
-                    id="loginMenu"
-                >
+                <font-awesome-icon :icon="['fas', 'bars']" @click="toggleVisibilty" />
+                <font-awesome-icon :icon="['fas', 'circle-user']" style="color: rgb(254 91 95 / 78%); padding-left: 9px"
+                    size="2xl" @click="toggleVisibilty" />
+                <div class="absolute z-50 rounded-md shadow-lg w-48 origin-top-right right-0 text-right p-3 bg-white mt-3"
+                    v-if="menuVisibility" id="loginMenu">
                     <!-- menu login -->
                     <div v-if="canLogin">
-                        <Link
-                            v-if="$page.props.auth.user"
-                            :href="route('dashboard')"
-                            class="font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >
-                            Dashboard
+                        <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                            class="font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        Dashboard
                         </Link>
                         <template v-else>
-                            <Link
-                                :href="route('login')"
-                                class="font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Log in
+                            <Link :href="route('login')"
+                                class="font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            Log in
                             </Link>
-                            <Link
-                                v-if="canRegister"
-                                :href="route('register')"
-                                class="ml-4 font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Register
+                            <Link v-if="canRegister" :href="route('register')"
+                                class="ml-4 font-semibold text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            Register
                             </Link>
                         </template>
                     </div>
@@ -88,6 +63,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../../scss/app.scss";
+
 #loginMenu a:hover {
     color: $main-color;
 }
