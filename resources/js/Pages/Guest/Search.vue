cb8837d58ec428ee214dcb05759d0e844d2909a8
Diletta Pelloni <dilpel@live.it>
2023-05-03 11:11:36 +0200

    sistemato ricerca

--- resources/js/Pages/Guest/Search.vue
+++ resources/js/Pages/Guest/Search.vue
@@ -189,16 +189,16 @@ export default {
             <form @submit.prevent="searchApartments" class="relative w-fit mx-auto ">
                 <!-- INDIRIZZO -->
                 <input type="text" id="addressInput" name="addressInput" placeholder="Inserisci un indirizzo..."
-                    v-model="addressInput" @input="getAutocompleteSearch()" class="me-3 rounded-full" autocomplete="off"/>
+                    v-model="addressInput" @input="getAutocompleteSearch()" class="me-3 rounded-full focus:ring-transparent focus:border-mainColor-300" autocomplete="off"/>
                 <button type="submit" class="button px-4 py-2 text-white rounded-full mt-5 sm:mt-0">
                     Cerca
                 </button>
 
                 <!-- ELENCO INDIRIZZI SUGGERITI -->
                 <div v-if="(addresses.length > 0) && (showAddresses)"
-                    class="absolute z-10 bg-white w-[500px] rounded-b-lg shadow-lg">
-                    <ul class="w-[500px]">
-                        <li v-for="address in addresses" :key="address.id" class="border-b border-gray-200 w-[500px]">
+                    class="absolute z-10 bg-white w-[200px] sm:w-[400px] md:w-[500px] rounded-lg shadow-lg overflow-hidden top-[150%] left-[50%] translate-x-[-50%]">
+                    <ul>
+                        <li v-for="address in addresses" :key="address.id" class="border-b border-gray-200">
                             <p class="hover:bg-gray-100 p-3" @click="selectAddress(address)">
                                 {{ address.address.freeformAddress }}
                             </p>
