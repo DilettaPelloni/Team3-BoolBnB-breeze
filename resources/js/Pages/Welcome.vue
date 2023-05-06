6ee9aa04ea01b1a158232c54df4d2c9e60bfeacc
LucaDalRi <euro2xd@gmail.com>
2023-05-06 15:09:21 +0200

    welcome tailwind 100%

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -30,7 +30,7 @@ export default {
         <link rel="icon" href="/favicon.svg" />
     </Head>
 
-    
+
 
     <AppHeader :canLogin="canLogin" :canRegister="canRegister" />
     <Jumbotron />
@@ -40,56 +40,38 @@ export default {
             <h2>Destinazioni suggerite</h2>
         </div><!-- CHIUSURA CONTENT -->
         <div
-                class="container-cards justify-items-center 2xl:mx-[160px] md:mx-[100px] grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-col-1 gap-10"
-            >
+            class="container-cards justify-items-center 2xl:mx-[160px] md:mx-[100px] grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-col-1 gap-10">
 
-                <div
-                    v-for="apartment in apartments"
-                    class="card flex flex-col rounded-[15px] overflow-hidden w-[274px] bg-white hover:shadow-lg hover:scale-105"
-                >
-                    <div class="w-full">
-                        <img
-                            :src="apartment.full_cover_img_path"
-                            alt="immagine casa"
-                            class="w-full aspect-square object-cover"
-                        />
-                    </div>
-                    <!-- CARD INFO -->
-                    <div
-                        class="card-info p-4 grow flex flex-col"
-                    >   
-                        <!-- TITOLO -->
-                        <div>
-                            <strong> {{ apartment.title }} </strong>
-                        </div>
-                        <!-- INDIRIZZO -->
-                        <div class="mt-2 grow flex items-start">
-                            <font-awesome-icon :icon="['fas', 'location-dot']" class="me-2 mt-1 text-mainColor-300" />
-                            {{ apartment.address }}
-                        </div>
-                    </div>
-                    <!-- BOTTONE -->
-                    <div class="text-center mb-5">
-                        <Link
-                            :href="route('guest_show', apartment.title_slug)"
-                            class="mt-8 px-4 py-2 rounded-full button bg-mainColor-300 text-white mb-[20px]"
-                        >
-                            Vedi appartamento
-                        </Link>
-                    </div>
-
+            <div v-for="apartment in apartments"
+                class="card flex flex-col rounded-[15px] overflow-hidden w-[274px] bg-white hover:shadow-lg hover:scale-105 transition-all ease-in-out duration-300">
+                <div class="w-full">
+                    <img :src="apartment.full_cover_img_path" alt="immagine casa"
+                        class="w-full aspect-square object-cover" />
                 </div>
-            </div><!-- CHIUSURA CARD CONTAINER -->
+                <!-- CARD INFO -->
+                <div class="card-info p-4 grow flex flex-col">
+                    <!-- TITOLO -->
+                    <div>
+                        <strong> {{ apartment.title }} </strong>
+                    </div>
+                    <!-- INDIRIZZO -->
+                    <div class="mt-2 grow flex items-start">
+                        <font-awesome-icon :icon="['fas', 'location-dot']" class="me-2 mt-1 text-mainColor-300" />
+                        {{ apartment.address }}
+                    </div>
+                </div>
+                <!-- BOTTONE -->
+                <div class="text-center mb-5">
+                    <Link :href="route('guest_show', apartment.title_slug)"
+                        class="mt-8 px-4 py-2 rounded-full button bg-mainColor-300 text-white mb-[20px] hover:bg-[#fd4e4e]">
+                    Vedi appartamento
+                    </Link>
+                </div>
+
+            </div>
+        </div><!-- CHIUSURA CARD CONTAINER -->
     </div><!-- CHIUSURA CONTAINER -->
     <AppFooter />
 </template>
 
-<style scoped lang="scss">
-.button:hover {
-    background-color: #fd4e4e;
-}
-.card {
-    transition: all 0.3s ease-in-out;
-}
-
-</style>
+<style scoped></style>
